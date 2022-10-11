330d410bdd9b5e94920e9ed6f092a818419b397b
Vinayak Palve <vinayak.palve01@gmail.com>
2022-10-11 17:30:53 +0530

    second commit

--- social_book/settings.py
+++ social_book/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['vinayak-sma.herokuapp.com','127.0.0.1']
 
 
 # Application definition
